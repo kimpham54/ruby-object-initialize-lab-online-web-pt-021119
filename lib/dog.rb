1d01acffab31b31fed6f130d81a7185ea58eb166
proactive-hashmap-4080 <kimpham54@gmail.com>
2019-04-02 04:41:59 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,9 +2,12 @@ class Dog
 attr_accessor :name
 attr_accessor :breed
 
-def initialize(name, breed="Mutt")
+def initialize(name, breed)
   @name = name
   @breed = breed
 end
+
+def breed
+  @breed
 
 end
